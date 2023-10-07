de4ff0ca9fd5c49687500bec49c683338b2682e4
oyare001 <adoyipeter11@gmail.com>
2023-10-06 18:03:26 -0700

    mumm

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints the alphabet in lowercase and uppercase.
+ * main - Program that prints the numbers from 0 to 9.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char lowercase, uppercase;
+    int number;
 
-    for (lowercase = 'a', uppercase = 'A'; lowercase <= 'z'; lowercase++, uppercase++)
+    for (number = 0; number <= 9; number++)
     {
-        putchar(lowercase);
-        putchar(uppercase);
+        putchar('0' + number);
     }
 
     putchar('\n');
